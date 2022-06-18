681d7fda6ea1727e9b6d69fb5c9692ace2b8c41c
Shubham <95shubham1@gmail.com>
2022-06-18 22:57:42 +0530

    descenth sum node

--- descendent sum.cpp
+++ descendent sum.cpp
@@ -3,10 +3,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void replaceWithSum(node *node){
+struct node
+{
+	int data;
+	struct node*left;
+	struct node*right;
+	node(int data){
+		this->data=data;
+		this->left=NULL;
+		this->right=NULL;
+	}
+};
+
+
+
+int replaceWithSum(node *node){
 	
 	if(node==NULL)
 	return 0;
+	
+	if(node->left==NULL && node->right==NULL)
+	return node->data;
+	
+	int  temp=node->data;
+	
+	node->data=replaceWithSum(node->left) + replaceWithSum(node->right);
+	
+	return temp+node->data;
 	
 }
 
