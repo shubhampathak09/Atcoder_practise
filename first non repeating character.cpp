90e1cd2a7c259760e980c03dd7ba149def321470
Shubham <95shubham1@gmail.com>
2022-06-18 18:34:39 +0530

    balanced brackets

---
+++ first non repeating character.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+string s;
+
+queue<char>q;
+
+map<char,int>mp;
+
+
+vector<char>result;
+
+result.push_back(s[0]);
+
+mp[s[0]]=1;
+
+for(int i=1;i<s.length();i++){
+	
+	
+}	
+	
+	
+}
+
+
