2e82654c8493ea0486f5e1ee67f8d2d2fadeefa0
Shubham <95shubham1@gmail.com>
2022-06-18 22:20:32 +0530

    diameter

---
+++ diameter of tree.cpp
@@ -0,0 +1,27 @@
+// Diameter of binary tree
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+	node*next;
+	int data;
+	
+	node(int data){
+		this->data=data;
+		this->next=NULL;
+	}
+};
+
+int height(node*root){
+	
+	
+}
+
+
+int main(){
+	
+	
+	
+}
