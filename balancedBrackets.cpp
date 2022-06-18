1b6bacab1b00274f383f87d9147e26df0309edba
Shubham <95shubham1@gmail.com>
2022-06-18 08:50:17 +0530

    balanced brackets

---
+++ balancedBrackets.cpp
@@ -0,0 +1,33 @@
+// balanced brackets
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void generateBalancedBrackets(int open,int close,int n,string bracket){
+	
+	if(open==n && close ==n){
+		
+		cout<<bracket<<endl;
+	}
+	
+	if(open<n){
+		string obracket=bracket;
+		obracket.push_back('(');
+		generateBalancedBrackets(open+1,close,n,obracket);
+		
+	}
+	
+	if(open>close){
+		string cbracket=bracket;
+		cbracket.push_back(')');
+		generateBalancedBrackets(open,close+1,n,cbracket);
+	}
+}
+
+int main(){
+	
+	int n=10;
+	string bracket="";
+	generateBalancedBrackets(0,0,n,bracket);
+	
+}
