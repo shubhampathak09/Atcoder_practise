2e42c0ebf8fbb5a1cf20db979e8f9fbd5e93888b
Shubham <95shubham1@gmail.com>
2022-06-18 20:04:04 +0530

    stock span problem

--- stock span.cpp
+++ stock span.cpp
@@ -1,3 +1,46 @@
 // stock span 
 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	vector<int>  inp ={100,80,60,70,60,75,85};
+	
+	vector<int> ngeL(inp.size());
+	
+	int n=inp.size();
+	
+	ngeL[0]=1;
+	
+	stack<int> s;
+	
+	s.push(0);
+	
+	for(int i=1;i<n;i++){
+		
+		while(!s.empty() && inp[i]>inp[s.top()]){
+			
+			s.pop();
+			
+		}
+		
+		if(s.empty()){
+			ngeL[i]=i+1;
+		}else
+		{
+		ngeL[i]=i - s.top();	
+		}
+		s.push(i);
+	}
+	
+	
+	for(int i=0;i<ngeL.size();i++){
+		
+		cout<<ngeL[i]<<" ";
+		
+	}
+	
+}
+
