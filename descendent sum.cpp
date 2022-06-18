19d99b3237a14b8ab6c5486c7cbe9af69861ec41
Shubham <95shubham1@gmail.com>
2022-06-18 22:48:15 +0530

    descenth sum node

---
+++ descendent sum.cpp
@@ -0,0 +1,22 @@
+// replace with descendant sum
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+node  * descendethSum(node *root){
+	
+	
+	if(root==NULL)
+	return 0;
+	
+	
+	
+	
+}
+
+int main(){
+	
+	
+	
+} 
