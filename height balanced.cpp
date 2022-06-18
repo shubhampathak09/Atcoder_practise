d4a6f521e7f1d0855e2f3f008c8a4bc92e12bf0c
Shubham <95shubham1@gmail.com>
2022-06-18 23:16:41 +0530

    height balanced

--- height balanced.cpp
+++ height balanced.cpp
@@ -5,6 +5,50 @@ using namespace std;
 
 struct node
 {
+	int data;
+	struct node*left;
+	struct node*right;
+	
+	
+}
+
+
+pair<int,bool> heightBalanced(struct node*root){
+	
+	
+	pair<int,bool> p;
+	
+	if(root==NULL)
+	{
+		p.first=0;
+		p.second=true;
+		return p;
+	}
+	
+	
+	pair<int,bool> left=heightBalanced(root->left);
+	pair<int,bool> right=heightBalanced(root->right)
+	
+	
+	p.first=max(left.first,right.first)+1;
+	
+	if(abs(left.first - right.first)<=1 && left.second==true && right.second==true){
+	p.second=true;	
+	}else
+	p.second=false;
+	
+	return p;
+	
+	//lsbt is heeight balanced
+	
+	// rsbt is height balanced
+	
+	
+	// difference in height of lssbt and rsbt is 1
+	
+	
+	
+	
 	
 }
 
