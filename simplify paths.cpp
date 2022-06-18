cbda08017d4b4821f63c8f728356fd79938918f2
Shubham <95shubham1@gmail.com>
2022-06-18 20:37:33 +0530

    collectiing deaatils

---
+++ simplify paths.cpp
@@ -0,0 +1,17 @@
+// simplify path
+#include<bits/stc++.h>
+using namespace  std;
+
+
+// cd ..  -> parent directoy
+// cd stacks -> got to stacks directoy
+// cd  ~/Desktop
+
+// / -> Root path
+/ /x/y/z
+
+int main(){
+	
+	
+	
+}
