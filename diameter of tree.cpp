7e3ec88d2a560e40415220c5a4ae0c2df4f0ecf4
Shubham <95shubham1@gmail.com>
2022-06-18 22:26:20 +0530

    diameter of tree

--- diameter of tree.cpp
+++ diameter of tree.cpp
@@ -5,23 +5,57 @@ using namespace std;
 
 struct node
 {
-	node*next;
 	int data;
-	
+	struct node*left;
+	struct node*right;
 	node(int data){
 		this->data=data;
-		this->next=NULL;
+		this->left=NULL;
+		this->right=NULL;
 	}
+	 
 };
 
 int height(node*root){
-	
+
+if(root==NULL)
+return 0;
+
+int h1=height(root->left);
+int h2=height(root->right);
+
+return max(h1,h2)+1;	
 	
 }
 
 
+int diameter(node*root){
+	
+	if(root==NULL)
+	return 0;
+	
+	int d1=height(root->left) + height(root->right);
+	
+	int d2=max(diameter(root->left),diameter(root->right));
+	
+	return max(d1,d2);
+	
+	
+	
+}
+
 int main(){
 	
+	struct node*root=new node(1);
+	root->left=new node(2);
+	root->right=new node(3);
+	root->left->left=new node(4);
+	root->left->right=new node(5);
+	root->left->right->left=new node(7);
 	
+	root->right->right=new node(6);
+	
+	
+	cout<<diameter(root);
 	
 }
