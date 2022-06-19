fa3f6727ad3c15c00576580618c0bc59ec63d027
Shubham <95shubham1@gmail.com>
2022-06-19 10:09:13 +0530

    infi transactions edge cases

--- buysellStockInfinteTransactionsAllowed.cpp
+++ buysellStockInfinteTransactionsAllowed.cpp
@@ -18,16 +18,13 @@ int maxProfit(vector<int>profit,int n){
 		}else
 		{
 			checkDip=true;
+			cout<<profit[sd]-profit[bd]<<endl;
 			profitT+= profit[sd] -profit[bd];
 			sd=bd=i;
 		}
 		
 	}
-	
-	if(checkDip==false)
-	{
-		return profit[n-1] -profit[0];
-	}
+	profitT+=profit[sd] -profit[bd]; 
 //	cout<<profitT;
 	return profitT;
 }
@@ -36,7 +33,7 @@ int maxProfit(vector<int>profit,int n){
 int main(){
 	
 	
-	vector<int>profit={1,2,3,4,5,6};
+	vector<int>profit={6,1,3,2,4,7};
 	
 	int n=profit.size();
 	
