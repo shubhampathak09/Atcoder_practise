8d02a912df3a90af412951880266eef4125c2f0b
Shubham <95shubham1@gmail.com>
2022-06-24 20:44:03 +0530

    fixed

---
+++ LIS binary search.cpp
@@ -0,0 +1,40 @@
+// lis using binary search approach
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+	
+
+
+vector<int> nums = {10,9,2,5,3,7,101,18};
+
+
+int n=nums.size();
+
+
+vector<int>temp;
+
+temp.push_back(nums[0]);
+	
+for(int i=1;i<n;i++){
+	
+	
+	if(nums[i]>temp.back()){
+		temp.push_back(nums[i]);
+	}else
+	{
+		int key=nums[i];
+		int index=lower_bound(temp.begin(),temp.end(),key)-temp.begin();
+		//cout<<index<<endl;
+		temp[index]=nums[i];
+		
+			}
+	
+}	
+
+cout<<temp.size();
+	
+}
