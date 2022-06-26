73eae0ceb9fd1c13e84d4815311ec36bb609683a
Shubham <95shubham1@gmail.com>
2022-06-26 11:41:59 +0530

    getting the last element of list

---
+++ src/Scala/LastElementOfLisy.scala
@@ -0,0 +1,12 @@
+package Scala
+
+
+
+object LastElementOfLisy extends App {
+
+  val myList : List[Int] =List(1,2,3,4,5,6,7)
+  val y = myList.last;
+
+  println("The Last element of list is.."+y)
+
+}
