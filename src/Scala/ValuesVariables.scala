b977ac34681cf75a66a4c39c19b063fa46759e2f
Shubham <95shubham1@gmail.com>
2022-07-01 22:30:27 +0530

    scala basics

---
+++ src/Scala/ValuesVariables.scala
@@ -0,0 +1,39 @@
+package Scala
+
+object ValuesVariables extends App{
+
+  val x =42
+  println(x)
+  // compiler can infer  types
+
+
+   // vals are imuttables
+
+  val aString :String ="Hello this is me"
+  // semicolons are not necessary
+
+  val aBoolean=false
+
+  val aChar :Char ='a'
+
+  val aInt :Int =2
+  val aShort :Short =0
+  val aLong : Long =4333333333334444444L
+
+  val aFloat : Float =2.0f
+  val aDouble :Double =3.14
+  // basic types
+
+  // variables
+
+  var aVariables:Int=2
+
+  aVariables=5
+  // variables  are used in side effects
+  // dislaying something on screen
+  //etc side effects cant be removed entirely
+
+  
+
+
+}
