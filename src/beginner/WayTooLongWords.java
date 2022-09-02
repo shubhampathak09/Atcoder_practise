f57ccef5b66c9602f5d89043634ab345fcf56eec
Shubham <95shubham1@gmail.com>
2022-09-02 23:22:57 +0530

    waytoolongstrings

--- src/beginner/WayTooLongWords.java
+++ src/beginner/WayTooLongWords.java
@@ -1,2 +1,25 @@
-package beginner;public class WayTooLongWords {
+package beginner;
+
+import java.util.Scanner;
+
+public class WayTooLongWords {
+    public static void main(String[] args){
+
+        Scanner s=new Scanner(System.in);
+
+        int n=s.nextInt();
+
+        while(n> 0){
+            String word=s.nextLine();
+            String newword="";
+            if(word.length()>10){
+                String wordApeend=Integer.toString(word.length()-2);
+                newword+=word.charAt(0) + wordApeend +word.charAt(word.length()-1);
+                word=newword;
+            }
+            n--;
+            System.out.println(word);
+        }
+
+    }
 }
