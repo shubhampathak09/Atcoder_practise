0674fe820e9d4e6c800c165425abdbdb9f41387b
Shubham <95shubham1@gmail.com>
2022-10-15 17:14:02 +0530

    fml

--- shrink an array removing consecurive triplets.cpp
+++ shrink an array removing consecurive triplets.cpp
@@ -4,19 +4,29 @@ using namespace std;
 
 
 int solve(vector<int>a,int k,int start,int end){
-	if(start>end){
+	
+	if(start > end){
 		return 0;
 	}
+	
 	int res=0;
 	
-	res = 1 +solve(a,k,start+1,end);
-	for(int i=start+1;i<end;i++){
-		for(int j=i+1;j<end;j++){
-			if(a[i]==a[start]+k && a[j]==a[i]+k){
-				if(!solve(a,k,start+1,i-1) && !solve(a,k,i+1,j-1)){
-					int n=solve(a,k,j+1,end);
-					if(res>n){
-						res=n;
+	res = 1 +solve(a,k,start+1,end); // a[start] does not form a triplet
+
+	for(int i=start+1;i<=end;i++){
+		
+		
+		for(int j = i+1;j <= end;j++){
+		
+			if(a[i] == a[start] + k && a[j] == a[i] + k){
+		        //cout<<i<<j<<endl; 
+				if(solve(a,k,start+1,i-1) != 0 && solve(a,k,i+1,j-1) != 0){
+		
+					int n= solve(a,k,j+1,end);
+	               // cout<<n<<endl;	
+					if(res > n){
+		
+						res = n;
 					}
 				}
 			}
@@ -26,9 +36,12 @@ int solve(vector<int>a,int k,int start,int end){
 }
 
 int main(){
-	vector<int> a={-1,2,5,8,2,5};
-    int k=3;
+	vector<int> a={-1,0,1,2,3,4};
+    
+	cout<<a.size();
 	
-	cout<<solve(a,k,0,a.size()-1);
+	int k=3;
+	
+	cout<<solve(a,k,0,5);
 		
 }
