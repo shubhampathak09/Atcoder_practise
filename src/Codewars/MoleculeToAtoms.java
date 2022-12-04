350c6726b8024db255910d6c7bbdff48709f9478
Shubham <95shubham1@gmail.com>
2022-12-04 11:08:04 +0530

    some generic exp

---
+++ src/Codewars/MoleculeToAtoms.java
@@ -0,0 +1,32 @@
+package Codewars;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class MoleculeToAtoms {
+
+    /**
+     * For a given chemical formula represented by a string,
+     * count the number of atoms of each element contained in the molecule
+     * and return an object (associative array in PHP, Dictionary<string, int> in C#, Map<String,Integer> in Java).
+     * @param args
+     */
+
+
+    /**
+     * String fremySalt = "K4[ON(SO3)2]2";
+     * parseMolecule.getAtoms(fremySalt); // return ["K": 4, "O": 14, "N": 2, "S": 4]
+     * @param args
+     */
+
+    public static Collection<Object[]> data(){
+        return Arrays.asList(new  Object[][]{
+
+        });
+    }
+
+    public static void main(String[] args){
+
+    }
+
+}
