0cd032f496d8c8355e7c3183b65eafd5a7f55bb1
Shubham <95shubham1@gmail.com>
2022-12-30 16:30:47 +0530

    backspce sring compare

---
+++ backspace.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	
+	string s="f##";
+	          
+	int idx=0;
+	
+	for(int i=0;i<s.length();i++){
+		if(s[i]!='#'){
+			s[idx]=s[i];
+			idx++;
+		}
+		else if(s[i]=='#' && idx >=0){
+			idx=idx-1;
+		}
+		if(idx < 0)
+		idx=0;
+	}
+	cout<<s.substr(0,idx);  //ab
+}
