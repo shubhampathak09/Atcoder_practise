153dd965bc6872d5f4fcf7e28a662e9ce40e31db
Shubham <95shubham1@gmail.com>
2023-01-06 20:44:22 +0530

    banjo

---
+++ src/beginner/Banjo.java
@@ -0,0 +1,16 @@
+package beginner;
+
+public class Banjo {
+    public static String areYouPlayingBanjo(String name) {
+        // Program me!
+        char c=name.charAt(0);
+        if(c=='r'||c=='R')
+            return name + " plays banjo" ;
+        else
+            return name + " does not play banjo";
+    }
+    
+}
+
+
+
