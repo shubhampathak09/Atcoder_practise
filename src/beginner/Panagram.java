3852d1b6a186f6c4167eb79541c1ffd93781cc8c
Shubham <95shubham1@gmail.com>
2023-01-07 20:38:12 +0530

    Lazy dog lamda solution

--- src/beginner/Panagram.java
+++ src/beginner/Panagram.java
@@ -16,9 +16,10 @@ public class Panagram {
             //code
         }
         public static void main(String[] args){
-            System.out.println(check("The quick brown fox jumps over the lay dg."));
+            System.out.println(check("The quick brown fox jumps over the lazy dg."));
             String s1="The quick brown fox jumps over the lay dg.";
             System.out.println("Method 2.."+   check1(s1));
+            System.out.println("Method 3.."+check3(s1));
         }
         public static  boolean check1(String Sentence){
             for(char c='a';c<='z';c++){
@@ -29,6 +30,12 @@ public class Panagram {
             return true;
         }
 
-        // Pangram checker  -> 793 to 801  codewars  
+        // Pangram checker  -> 793 to 801  codewars
+
+      //Using Streams and Filter
+
+      public static boolean check3(String sentence){
+            return sentence.chars().filter(Character::isLetter).map(Character::toLowerCase).distinct().count()==26;
+      }
 
 }
