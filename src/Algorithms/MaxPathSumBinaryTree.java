a4762eea5549d8e56194fe77ba28dee8c74b177a
Shubham <95shubham1@gmail.com>
2023-01-27 20:47:25 +0530

    maxVal

--- src/Algorithms/MaxPathSumBinaryTree.java
+++ src/Algorithms/MaxPathSumBinaryTree.java
@@ -13,16 +13,17 @@ public class MaxPathSumBinaryTree {
     }
     public static int maxval;
 
-    public static int findMaxUtil(node node,int maxval)
+    public static int findMaxUtil(node node,int mxval)
     {
         if(node==null)
             return 0;
 
-        int l=findMaxUtil(node.left,maxval);
-        int r=findMaxUtil(node.right,maxval);
+        int l=findMaxUtil(node.left,mxval);
+        int r=findMaxUtil(node.right,mxval);
         int max_single=Math.max(Math.max(l,r)+node.data,node.data);
         int max_top=Math.max(max_single,l+r+node.data);
-        maxval=Math.max(maxval,max_top);
+        mxval=Math.max(mxval,max_top);
+        maxval=mxval;
         return max_single;
     }
 
@@ -47,7 +48,7 @@ root.right.right.right=new node(4);
 
     System.out.println("Printing the Structure of tree for debugging purpose only");
     inorder(root);
-    maxval=0;
+    maxval=Integer.MIN_VALUE;
     int sum=findMaxUtil(root,maxval);
 
   //  System.out.println(sum);
