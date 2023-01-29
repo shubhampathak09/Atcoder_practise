7717e38b9a70dd96b16e7abd3909cf51491022b6
Shubham <95shubham1@gmail.com>
2023-01-29 19:43:30 +0530

    collectors and list in java streams

--- src/streams/findFirstOdd.java
+++ src/streams/findFirstOdd.java
@@ -1,6 +1,9 @@
 package streams;
 
+import java.net.Inet4Address;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class findFirstOdd {
@@ -13,7 +16,14 @@ public class findFirstOdd {
                 .filter(i -> i % 2 !=0)
                 .findFirst()
                 .orElse(Integer.MIN_VALUE);
-
+/*
+        List<Integer> lis=Stream.of(arr)
+                        .flatMapToInt(Arrays::stream)
+                                .filter(o -> o % 2!=0)
+                                        .collect(Collectors.toList());
+  */
+        List<Integer>lis=Arrays.asList(2,2,4,8,1,3,13,5,6,9,10).stream().filter(o -> o % 2!=0).collect(Collectors.toList());
+        System.out.println(lis);
         System.out.println(oddOrMin);
     }
 }
