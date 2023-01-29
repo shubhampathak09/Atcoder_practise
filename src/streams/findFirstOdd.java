cc249602a6d045d80f48a05c3a627ab928c7045c
Shubham <95shubham1@gmail.com>
2023-01-29 19:45:53 +0530

    streams

--- src/streams/findFirstOdd.java
+++ src/streams/findFirstOdd.java
@@ -10,20 +10,19 @@ public class findFirstOdd {
     public static void main(String[] args){
         int[] arr={2,6,7,1,0,9,10,0,3};
 
-
+       /** find first odd from integer array using streams **/
         int oddOrMin= Stream.of(arr)
                 .flatMapToInt(Arrays::stream)
                 .filter(i -> i % 2 !=0)
                 .findFirst()
                 .orElse(Integer.MIN_VALUE);
-/*
-        List<Integer> lis=Stream.of(arr)
-                        .flatMapToInt(Arrays::stream)
-                                .filter(o -> o % 2!=0)
-                                        .collect(Collectors.toList());
-  */
+
+
+        /**2. group all odd integers together in a list **/
         List<Integer>lis=Arrays.asList(2,2,4,8,1,3,13,5,6,9,10).stream().filter(o -> o % 2!=0).collect(Collectors.toList());
         System.out.println(lis);
         System.out.println(oddOrMin);
+
+        
     }
 }
