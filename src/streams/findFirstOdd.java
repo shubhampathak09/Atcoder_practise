c78e2d09d30bcac0eb1baf732e2077c4b5b0833c
Shubham <95shubham1@gmail.com>
2023-01-29 19:52:27 +0530

    streams more example

--- src/streams/findFirstOdd.java
+++ src/streams/findFirstOdd.java
@@ -19,10 +19,19 @@ public class findFirstOdd {
 
 
         /**2. group all odd integers together in a list **/
-        List<Integer>lis=Arrays.asList(2,2,4,8,1,3,13,5,6,9,10).stream().filter(o -> o % 2!=0).collect(Collectors.toList());
+        List<Integer>lis=Arrays.asList(2,2,4,8,1,3,13,5,6,9,10,13,17,19,100,101,4).stream().filter(o -> o % 2!=0).collect(Collectors.toList());
         System.out.println(lis);
         System.out.println(oddOrMin);
 
+        /**3. print all even numbers from stream **/
+        List<Integer>sample=Arrays.asList(2,2,4,8,1,3,13,5,6,9,10,13,17,19,100,101,4);
+        List<Integer>even=sample.stream().filter(o -> o % 2 ==0).collect(Collectors.toList());
+        System.out.println(even);
+
+        /**4.program to print even numbers from an array**/
+        Arrays.stream(arr).filter(x -> x%2==0).forEach(System.out::print);
+
         
+
     }
 }
