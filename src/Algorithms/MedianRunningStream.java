5db9d70853e7233b815fb34ad0e867b73488b0c1
Shubham <95shubham1@gmail.com>
2023-01-30 21:34:38 +0530

    median in running stream optimised approach using heaps

--- src/Algorithms/MedianRunningStream.java
+++ src/Algorithms/MedianRunningStream.java
@@ -1,11 +1,60 @@
 package Algorithms;
 
+import scala.Int;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class MedianRunningStream {
     /** optimised appraoch using heaps asked in nvidia **/
 
-    public static void add(int val){}
+    public static PriorityQueue<Integer> left=new PriorityQueue(Collections.reverse());
+    public static PriorityQueue<Integer> right =new PriorityQueue();
 
+    public static void add(int val){
+        if(right.size()>0 && val> right.peek())
+            right.add(val);
+        else
+            left.add(val);
 
+        if(left.size()- right.size()==2){
+            right.add(left.remove());
+        }else if(right.size()- left.size()==2){
+            left.add(right.remove());
+        }
+    }
+
+    public static int remove(){
+
+        if(size()==0)
+        {
+            System.out.println("Cant Remove empty heap, since no elements are present in stream");
+            return -1;
+        }
+        if(left.size()>=right.size()){
+            return left.remove();
+        }else
+        {
+            return right.remove();
+        }
+
+    }
+    public static int getMedian(){
+
+        if(size()==0){
+            System.out.println("Cant Retrieve from empty heap, since no elements are present in stream");
+            return -1;
+        }
+
+        if(left.size()>=right.size()){
+            return left.peek();
+        }else {
+            return right.peek();
+        }
+    }
+    public static int size(){
+     return left.size()+right.size();
+    }
     public static void main(String[]args){
 
     }
