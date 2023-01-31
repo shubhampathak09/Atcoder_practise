e13ee0c75770e8ae9728538a2f3c81f20ae16c99
Shubham <95shubham1@gmail.com>
2023-01-31 20:43:51 +0530

    count duplicates code wars 6kyu

---
+++ src/Algorithms/CountingDuplicates.java
@@ -0,0 +1,20 @@
+package Algorithms;
+
+public class CountingDuplicates {
+
+    /**problem satement**/
+    /**
+    Count the number of Duplicates
+    Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string.
+    The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+    **/
+
+    public static int duplicateCount(String text) {
+        // Write your code here
+    }
+
+    public static void  main(String[] args){
+
+    }
+
+}
