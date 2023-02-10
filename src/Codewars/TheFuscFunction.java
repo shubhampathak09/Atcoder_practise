e5f121a859265517f5b4bc3fbd47bac820df4e03
Shubham <95shubham1@gmail.com>
2023-02-10 23:06:53 +0530

    The Fusc Problem

--- src/Codewars/TheFuscFunction.java
+++ src/Codewars/TheFuscFunction.java
@@ -5,29 +5,30 @@ import java.math.BigInteger;
 public class TheFuscFunction {
 
     /**
-    This Kata is a continuation of Part 1. The fusc function is defined recursively as follows:
+     * This Kata is a continuation of Part 1. The fusc function is defined recursively as follows:
+     * <p>
+     * fusc(0) = 0
+     * fusc(1) = 1
+     * fusc(2n) = fusc(n)
+     * fusc(2n + 1) = fusc(n) + fusc(n + 1)
+     * Your job is to produce the code for the fusc function. In this kata, your function will be tested with large values of n more than 1000 bits ( in JS, LC and PHP: at most 52 bits, in OCaml: at most 64 bits ), so you should be concerned about stack overflow and timeouts.
+     * <p>
+     * Method suggestion:
+     * <p>
+     * Imagine that instead of fusc(n), you were to implement fib(n), which returns the n'th Fibonacci number.
+     * <p>
+     * The function is recursively defined by:
+     **/
 
-    fusc(0) = 0
-    fusc(1) = 1
-    fusc(2n) = fusc(n)
-    fusc(2n + 1) = fusc(n) + fusc(n + 1)
-    Your job is to produce the code for the fusc function. In this kata, your function will be tested with large values of n more than 1000 bits ( in JS, LC and PHP: at most 52 bits, in OCaml: at most 64 bits ), so you should be concerned about stack overflow and timeouts.
 
-    Method suggestion:
-
-    Imagine that instead of fusc(n), you were to implement fib(n), which returns the n'th Fibonacci number.
-
-    The function is recursively defined by:
-    **/
-
-    public class Fusc {
-        public static BigInteger fusc (BigInteger n) {
+        public static BigInteger fusc(BigInteger n) {
 
             return n;
         }
 
-    public static void main(String[] args){
+        public static void main(String[] args) {
 
 
+        }
     }
-}
+
