1c922e41423763e971a6544bf38cc2f222b0239f
Shubham <95shubham1@gmail.com>
2023-02-15 19:21:16 +0530

    working on my thread counter

--- src/ThreadCount/ThreadCounting.java
+++ src/ThreadCount/ThreadCounting.java
@@ -1,26 +1,31 @@
 package ThreadCount;
 
 public class ThreadCounting {
-    private static final int constantDiff=3;
+    private static final int constantDiff = 3;
 
 
-    class CounterThread extends Thread{
+}
+    class CounterThread extends Thread {
         private Counter counter;
         private Integer constantDifference;
         private Integer initialValue;
         private boolean done;
         private boolean canCallCount;
-        CounterThread(Counter counter,Integer constantDifference,Integer initialValue){
-            this.counter=counter;
-            this.constantDifference=constantDifference;
-            this.initialValue=initialValue;
+
+        CounterThread(Counter counter, Integer constantDifference, Integer initialValue) {
+            this.counter = counter;
+            this.constantDifference = constantDifference;
+            this.initialValue = initialValue;
         }
-        public void setDone(boolean done){
-            this.done=done;
+
+        public void setDone(boolean done) {
+            this.done = done;
         }
-        public boolean isDone(){
+
+        public boolean isDone() {
             return done;
         }
+
         /*
          * This flag is used to control which Thread is able to take control of the lock
          * when it becomes available.
@@ -29,6 +34,30 @@ public class ThreadCounting {
          * be possible that thread2 acquires control of the lock, resulting in the wrong
          * thread calling counter.count(i). -ok
          */
+        public void setCanCallCount(boolean canCallCount) {
+            this.canCallCount = canCallCount;
+        }
 
+        public boolean canCallCount() {
+            return canCallCount;
+        }
+
+        @Override
+        public void run() {
+            int i = initialValue;
+            while (isDone()) {
+                if (canCallCount()) {
+                    synchronized (counter) {
+                        counter.count(i);
+                        counter.notify();
+                    }
+                    i += constantDifference;
+                    setCanCallCount(false);
+                    if (i > 100) {
+                        setDone(true);
+                    }
+                }
+            }
+        }
     }
-}
+
