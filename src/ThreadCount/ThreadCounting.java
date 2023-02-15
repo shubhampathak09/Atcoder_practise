21d9090e461d81b690862a1da3ee7822eff53a2c
Shubham <95shubham1@gmail.com>
2023-02-15 19:24:04 +0530

    testing the logic adding synchronised locks

--- src/ThreadCount/ThreadCounting.java
+++ src/ThreadCount/ThreadCounting.java
@@ -3,6 +3,35 @@ package ThreadCount;
 public class ThreadCounting {
     private static final int constantDiff = 3;
 
+    public static void countInThreads(Counter counter){
+        CounterThread counterThread1=new CounterThread(counter,constantDiff,1);
+        CounterThread counterThread2=new CounterThread(counter,constantDiff,2);
+        CounterThread counterThread3=new CounterThread(counter,constantDiff,3);
+
+        counterThread1.start();
+        counterThread2.start();
+        counterThread3.start();
+
+        /*
+         * The main control loop.
+         *
+         * This (main) thread is responsible for
+         *  - iterating through from 1 to 100
+         *  - informing a given thread that it is OK to proceed (to call counter.count(i))
+         *  - Synchronising the activities of each counter thread and itself.
+         *
+         *  For each integer between 1 and 100 inclusive.
+         *      Inform the relevant counter thread, that it is OK to go.
+         *      Execute a synchronised wait.
+         *
+         *      note: each thread is responsible for assisting in this synchronisation by
+         *      1) releasing the lock on the monitor.
+         *      2) resetting value so that it is not OK to go (it's work is done for the moment
+         *        until the main loop determines otherwise).
+         *
+         */
+
+    }
 
 }
     class CounterThread extends Thread {
