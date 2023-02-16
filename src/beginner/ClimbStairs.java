9d6fa540cfcfd5ea3136484751192c48a5746b5d
Shubham <95shubham1@gmail.com>
2023-02-16 08:09:49 +0530

    climb stairs memoised optimisation

--- src/beginner/ClimbStairs.java
+++ src/beginner/ClimbStairs.java
@@ -6,20 +6,23 @@ public class ClimbStairs {
     public static void main(String[] args){
         Scanner s= new Scanner(System.in);
         int n=s.nextInt();
-        int c=countPaths(n);
+        int c=countPaths(n,new int[n+1]);
+        System.out.println(c);
     }
     /** 3^m complexity **/
-    public static  int countPaths(int n){
+    public static  int countPaths(int n,int [] qb){
 
-        if(n<= 0)
+        if(n< 0)
             return 0;
         else if(n==0)
             return 1;
-
-        int nx1=countPaths(n-1);
-        int nx2=countPaths(n-2);
-        int nx3=countPaths(n-3);
+        if(qb[n]!=0)
+            return qb[n];
+        int nx1=countPaths(n-1,qb);
+        int nx2=countPaths(n-2,qb);
+        int nx3=countPaths(n-3,qb);
         int nx0=nx1 +nx2 +nx3;
+        qb[n]=nx0;
         return nx0;
     }
 }
