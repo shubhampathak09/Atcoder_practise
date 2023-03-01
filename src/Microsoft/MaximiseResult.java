7a0523ba3cab211d489ca62f3ae03c5c6ef0c2fb
Shubham <95shubham1@gmail.com>
2023-03-01 11:33:51 +0530

    maximisime sum by replacing exactly once

--- src/Microsoft/MaximiseResult.java
+++ src/Microsoft/MaximiseResult.java
@@ -5,9 +5,36 @@ public class MaximiseResult {
 
    public String removeDigit(String number,char digit){
 
+       int index=-1;
+       for(int i=0;i<number.length();i++){
+           if(number.charAt(i)==digit && number.charAt(i) - '0' < number.charAt(i+1) -'0'){
+               index =i;
+               break;
+           }
+       }
+       if(index ==-1){
+           for(int i=number.length()-1;i>=0;i--){
+               if(number.charAt(i)==digit){
+                   index=i;
+                   break;
+               }
+           }
+       }
+
+       String ans="";
+       for(int i=0;i<number.length();i++){
+           if(i!=index){
+               ans+=number.charAt(i);
+           }
+       }
+       return ans;
    }
 
     public static void main(String[] args){
+
+       String  number="123";
+       char digit='3';
+
 
     }
 }
