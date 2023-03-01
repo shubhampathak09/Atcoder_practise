f27c8ac5382319673d60af0d8bfbca2bd2f120c4
Shubham <95shubham1@gmail.com>
2023-03-01 11:33:09 +0530

    Maximise

---
+++ src/Microsoft/MaximiseResult.java
@@ -0,0 +1,13 @@
+package Microsoft;
+
+public class MaximiseResult {
+   /** https://leetcode.com/problems/remove-digit-from-number-to-maximize-result/  **/
+
+   public String removeDigit(String number,char digit){
+
+   }
+
+    public static void main(String[] args){
+
+    }
+}
