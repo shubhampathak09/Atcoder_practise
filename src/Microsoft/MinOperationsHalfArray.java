a5c9347dab327cbebb8d89e237f28da0a9406a18
Shubham <95shubham1@gmail.com>
2023-03-01 11:41:08 +0530

    adding ref link

--- src/Microsoft/MinOperationsHalfArray.java
+++ src/Microsoft/MinOperationsHalfArray.java
@@ -1,4 +1,9 @@
 package Microsoft;
 
 public class MinOperationsHalfArray {
+    /**https://leetcode.com/problems/minimum-operations-to-halve-array-sum/ **/
+
+    public static void main(String[] args){
+
+    }
 }
