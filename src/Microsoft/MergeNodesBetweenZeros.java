e181e7183d30bc160114cc4515c630763c10ea4a
Shubham <95shubham1@gmail.com>
2023-03-02 21:44:35 +0530

    sum of nodes b/w consective zeros

--- src/Microsoft/MergeNodesBetweenZeros.java
+++ src/Microsoft/MergeNodesBetweenZeros.java
@@ -10,10 +10,39 @@ public class MergeNodesBetweenZeros {
         }
 
     }
-
+    static void printList(ListNode head){
+        while(head.next!=null){
+            System.out.print(head.val + "->");
+            head=head.next;
+        }
+        System.out.println(head.val);
+    }
+    static void inPlaceStore(ListNode head){
+        if(head.val==0){
+            head=head.next;
+        }
+        ListNode res=head;
+        int sum=0;
+        ListNode temp=head;
+        while(temp!=null){
+            if(temp.val!=0){
+                sum+=temp.val;
+                temp=temp.next;
+            }else{
+                res.val=sum;
+                res.next=temp.next;
+                temp=res.next;
+                res=res.next;
+            }
+        }
+        printList(head);
+    }
     public static ListNode mergeNodes(ListNode head) {
 
         return null;
+    }
+    public static void main(String[] args){
+
     }
 
 }
