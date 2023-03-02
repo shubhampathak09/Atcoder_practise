3b3d889feb7db79c989972453bb15ec54fa980f8
Shubham <95shubham1@gmail.com>
2023-03-02 09:41:13 +0530

    add nodes between consecutive zeroes

---
+++ src/Microsoft/MergeNodesBetweenZeros.java
@@ -0,0 +1,19 @@
+package Microsoft;
+
+public class MergeNodesBetweenZeros {
+
+    public static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int val) {this.val=val;
+        this.next=null;
+        }
+
+    }
+
+    public static ListNode mergeNodes(ListNode head) {
+
+        return null;
+    }
+
+}
