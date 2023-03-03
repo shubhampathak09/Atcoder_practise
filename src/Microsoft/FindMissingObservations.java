f9315c897696233b2e780538c24043b227ff0116
Shubham <95shubham1@gmail.com>
2023-03-03 08:36:38 +0530

    added some  todo

--- src/Microsoft/FindMissingObservations.java
+++ src/Microsoft/FindMissingObservations.java
@@ -1,14 +1,40 @@
 package Microsoft;
 
+import scala.Int;
+
+import java.util.ArrayList;
+
 public class FindMissingObservations {
     public static int[] missingRolls(int[] rolls, int mean, int n) {
 
+        return new int[]{2,3,4};
 
+
+    }
+
+    public static ArrayList<Integer> findArray(int S, int n,ArrayList<Integer>result)
+    {
+        if(S==0){
+            return result;
+        }
+        return result;
     }
     public static void main(String[] args){
       int[] rolls={3,2,4,3};
       int mean=4;
       int n=2;
+      int m=rolls.length;
+
+//      12 + s / 2 + 4  =   12 +s / 6  = 4
+//
+//              12 +s =24
+//
+//                 s=12
+//                 int arr= {6,6};
+//                 int arr = 5,3
+
+     //   [S into n using 1 to 6]
+
 
     }
 }
