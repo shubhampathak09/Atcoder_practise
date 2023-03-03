e90d4d93921f33820b945c3ed76886565374aa20
Shubham <95shubham1@gmail.com>
2023-03-03 08:23:27 +0530

    Find missing observations

---
+++ src/Microsoft/FindMissingObservations.java
@@ -0,0 +1,14 @@
+package Microsoft;
+
+public class FindMissingObservations {
+    public static int[] missingRolls(int[] rolls, int mean, int n) {
+
+
+    }
+    public static void main(String[] args){
+      int[] rolls={3,2,4,3};
+      int mean=4;
+      int n=2;
+
+    }
+}
