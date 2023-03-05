c1af179a55aef0b3248582214aa4ac9ff7d19e35
Shubham <95shubham1@gmail.com>
2023-03-05 19:58:37 +0530

    3 divs code non optimised

--- src/Microsoft/ThreeDivisors.java
+++ src/Microsoft/ThreeDivisors.java
@@ -1,4 +1,44 @@
 package Microsoft;
 
 public class ThreeDivisors {
+
+   public static boolean isPrime(int n){
+       if(n==2)
+       {
+           return true;
+       }
+       for(int i=2;i<n;i++){
+           if(n % i ==0){
+               return false;
+           }
+       }
+       return true;
+   }
+
+   public static void main(String[] args){
+       /** check if a number has exactly three divisors **?
+        *
+        */
+
+
+       /**
+        *
+        * The number must be prime number * prime number and perfect square
+        */
+
+       int n=100;
+
+       double p=Math.sqrt(100);
+
+       if(p * p == (double) n){
+           if(isPrime((int)p)){
+               System.out.println(n +" has 3 factors exactly");
+           }else
+           {
+               System.out.println(n +" does not have 3 factors exactly");
+           }
+       }
+
+   }
+
 }
