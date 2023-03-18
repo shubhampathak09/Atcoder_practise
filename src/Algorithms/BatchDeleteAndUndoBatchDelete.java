4eee46167103e7e6977642e91fd01c55a58974d5
Shubham <95shubham1@gmail.com>
2023-03-18 22:13:44 +0530

    tk  system design problem

---
+++ src/Algorithms/BatchDeleteAndUndoBatchDelete.java
@@ -0,0 +1,15 @@
+package Algorithms;
+
+
+/** Attempt a tiktok
+ * system design question
+ * system design batch delete and recover system
+ *
+ *
+ * **/
+
+
+
+
+public class BatchDeleteAndUndoBatchDelete {
+}
