d3df07512691d433cf71d13bb86ca8de027babbd
Shubham <95shubham1@gmail.com>
2023-03-31 22:46:07 +0530

    testing stacks basics

---
+++ src/Stack/IntroStack.java
@@ -0,0 +1,27 @@
+package Stack;
+
+import java.util.Stack;
+
+public class IntroStack {
+
+    public static void main(String[] args){
+        Stack<Integer>st=new Stack<>();
+
+                st.push(10);
+        st.push(20);
+        System.out.println(st);
+
+        st.push(30);
+        System.out.println(st);
+        st.push(40);
+        System.out.println(st);
+        System.out.println(st.peek() +" "+st.size());
+        st.pop();
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+    st.pop();
+        System.out.println(st);
+    }
+
+}
