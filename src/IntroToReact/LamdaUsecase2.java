2a36534539f6158ea695d24009475340820c3b8d
Shubham <95shubham1@gmail.com>
2023-04-30 23:21:34 +0530

    operation multiply and add

--- src/IntroToReact/LamdaUsecase2.java
+++ src/IntroToReact/LamdaUsecase2.java
@@ -3,10 +3,16 @@ package IntroToReact;
 public class LamdaUsecase2 {
 
     public static void main(String[] args){
-    MathOperation operation= (a,b) ->
+    MathOperation addOperation= (a,b) ->
         System.out.println(a+b);
 
-    operation.operation(2,3);
+    addOperation.operation(2,3);
+
+
+    MathOperation mulOperation = (a,b) -> System.out.println(a * b);
+
+    mulOperation.operation(2,3);
+
     }
 
 }
