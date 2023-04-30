444d8f50f62844ab091b425a7ec145668cb2bbeb
Shubham <95shubham1@gmail.com>
2023-04-30 22:43:04 +0530

    invoking functinal interface

--- src/IntroToReact/FunctionalIntercaceDemo.java
+++ src/IntroToReact/FunctionalIntercaceDemo.java
@@ -2,11 +2,16 @@ package IntroToReact;
 
 public class FunctionalIntercaceDemo {
     public static void main(String[] args){
-        FunctionalInterface myInterface =
-                () -> System.out.println("hello world");
+//        FunctionalInterface myInterface =
+//                () -> System.out.println("hello world");
+//
+//        myInterface.myMethod();
 
-        myInterface.myMethod();
+         onTheFly(() -> System.out.println("helloWorld"));
+    }
 
+    public static void onTheFly(FunctionalInterface fun){
+        fun.myMethod();
     }
 
 }
