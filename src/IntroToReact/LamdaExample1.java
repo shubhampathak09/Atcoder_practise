ebf3fde9c16b1d9924a8f8f2ebe721a189b9b937
Shubham <95shubham1@gmail.com>
2023-04-30 22:32:37 +0530

    converting into lamda

--- src/IntroToReact/LamdaExample1.java
+++ src/IntroToReact/LamdaExample1.java
@@ -2,12 +2,11 @@ package IntroToReact;
 
 public class LamdaExample1 {
     public static void main(String[] args){
-        Thread t =new Thread(new Runnable() {
-            @Override
-            public void run() {
-             System.out.println("Thread Executed");
-            }
-        });
+        Thread t =new Thread(
+
+            () ->
+             System.out.println("Thread Executed")
+        );
         t.run();
     }
 }
