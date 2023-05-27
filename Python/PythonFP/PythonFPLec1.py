cd33c18d119daf7b6a6c9e13c873c6a93cc3624a
Shubham <95shubham1@gmail.com>
2023-05-27 09:54:36 +0530

    adding some function lamdas

--- Python/PythonFP/PythonFPLec1.py
+++ Python/PythonFP/PythonFPLec1.py
@@ -12,3 +12,9 @@ Created on Sat May 27 09:45:21 2023
 multiple_3_5=lambda x : x % 3 == 0 or x % 5 == 0
 
 
+# recursive def for sum of sequences
+
+def sum(seq):
+    if(len(seq) == 0): return 0
+    return seq[0] + sum(seq[1:])
+
