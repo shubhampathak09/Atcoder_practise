7d9685626057203213ebc97366d5718a61b4e5c1
Shubham <95shubham1@gmail.com>
2023-05-27 09:50:33 +0530

    init commit py

---
+++ Python/IceCube-Neutrios Deep in Ice/untitled0.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 27 09:45:21 2023
+
+@author: 91954
+"""
+
+## Python Functional Programming book  -> packt 
---
+++ Python/PythonFP/PythonFPLec1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 27 09:45:21 2023
+
+@author: 91954
+"""
+
+## Python Functional Programming book  -> packt 
+
+#simple lamda expression
+
+multiple_3_5=lambda x : x % 3 == 0 or x % 5 == 0
+
+
