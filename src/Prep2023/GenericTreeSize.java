4dab65ffbc7a27095ed156c1ff307b666d24a99f
Shubham <95shubham1@gmail.com>
2023-06-08 12:30:32 +0530

    gt display

--- src/Prep2023/GenericTreeSize.java
+++ src/Prep2023/GenericTreeSize.java
@@ -16,11 +16,26 @@ public class GenericTreeSize {
         return 0;
     }
 
+    public static void display(Node node){
+
+        String str =node.data + " -> ";
+        for(Node child : node.children){
+            str += child.data + " ,";
+        }
+            str += ".";
+        System.out.println(str);
+
+        for(Node child : node.children){
+            display(child);
+        }
+
+    }
+
     public static void main(String[] args){
         int[] arr ={10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,
         -1,-1,90,-1,-1,40,100,-1,-1,-1};
 
-        Node root;
+        Node root=null;
 
         Stack<Node> st = new Stack();
         for(int i=0 ;i < arr.length; i++){
@@ -39,6 +54,7 @@ public class GenericTreeSize {
             }
 
         }
+        display(root);
     }
 
 }
