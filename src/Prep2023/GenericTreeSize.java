ae79d60dfb26053d57105b78ecb99c4b9a57e515
Shubham <95shubham1@gmail.com>
2023-06-08 11:58:24 +0530

    gt

---
+++ src/Prep2023/GenericTreeSize.java
@@ -0,0 +1,22 @@
+package Prep2023;
+
+import java.util.ArrayList;
+
+public class GenericTreeSize {
+
+
+    private class Node{
+        int data;
+        ArrayList<Node> children =new ArrayList<>();
+    }
+
+    public static int size(Node node){
+        int s=0;
+        return 0;
+    }
+
+    public static void main(String[] args){
+
+    }
+
+}
