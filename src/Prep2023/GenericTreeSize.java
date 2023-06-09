8a1f3505b0d5c9f16d2b22695b66d8dc6ffe808c
Shubham <95shubham1@gmail.com>
2023-06-09 10:25:59 +0530

    level order algo

--- src/Prep2023/GenericTreeSize.java
+++ src/Prep2023/GenericTreeSize.java
@@ -78,6 +78,12 @@ public static void traversals(Node node){
 
 }
 
+public static void levelOrder(Node node){
+      // queue ds
+    // algo
+    // remove print add children
+}
+
     public static void main(String[] args){
         int[] arr ={10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,
         -1,-1,90,-1,-1,40,100,-1,-1,-1};
