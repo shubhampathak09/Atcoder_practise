db8719e44f5a05f0c56b4ca2d9b26198ca3108c6
Shubham <95shubham1@gmail.com>
2023-06-11 12:40:34 +0530

    reacctive

---
+++ src/Reactive/Intro.java
@@ -0,0 +1,19 @@
+package Reactive;
+
+public class Intro {
+
+    // paradigm shift
+
+  // concerend with data sstream, propagation of change
+
+    // st ds, dy ds
+
+    // async ?? -> one afer an another, things happeign all over the plaes
+
+    // reactive ! = asyn you can have syn has as well
+
+    // deal with streams of data
+
+    // fp
+
+}
