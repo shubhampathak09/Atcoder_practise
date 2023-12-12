e3cc2935e49e5825d90e90965f6ab779adbd98e8
Shubham <95shubham1@gmail.com>
2023-12-12 15:39:50 +0530

    Leetcode Dec 11,2023, problem

---
+++ src/Daily/FindSpecialInteger.java
@@ -0,0 +1,34 @@
+package Daily;
+
+public class FindSpecialInteger {
+
+    public static int findSpecialInteger(int [] arr){
+        int l=arr.length;
+        int qtr = arr.length/4;
+        int a = arr[0];
+
+        int cnt = 1;
+        for(int i=1;i<arr.length;i++){
+            if(a == arr[i])
+                cnt++;
+            if(cnt > qtr) return arr[i];
+
+            a = arr[i];
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+
+        int [] arr = {1,2,2,6,6,6,6,7,10};
+
+        int l=arr.length;
+
+        int ans = findSpecialInteger(arr);
+
+        System.out.println(ans);
+
+    }
+
+}
--- src/Daily/MaximiseProductOfTwo.java
+++ src/Daily/MaximiseProductOfTwo.java
@@ -1,5 +1,7 @@
 package Daily;
 
+import Daily.recursion.MergeIntervals;
+
 import java.util.Arrays;
 
 public class MaximiseProductOfTwo {
@@ -10,13 +12,30 @@ public class MaximiseProductOfTwo {
         return (nums[l-1] -1) * (nums[l-2] -1);
     }
 
+
+    public static int optimisedMaxProduct(int[] nums){
+
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        for(int num : nums){
+            if(num >= max1){
+                max2 = max1;
+                max1 = num;
+            } else if(num >max2){
+                max2 = num;
+            }
+        }
+        return (max1-1) *(max2 -1);
+    }
+
     public static void main(String[] args){
 
         int nums[]={3,4,5,2};
 
         int ans = maxProduct(nums);
-
-        System.out.println(ans);
+        int ans2 = optimisedMaxProduct(nums);
+        System.out.println(ans2);
 
     }
 
