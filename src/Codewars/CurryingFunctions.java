65c8059b41aa10ac7512cb0db906232f9dc3dab7
Shubham <95shubham1@gmail.com>
2023-12-16 20:18:19 +0530

    SCC code some bugs will fix

---
+++ src/Codewars/CurryingFunctions.java
@@ -0,0 +1,19 @@
+package Codewars;
+
+public class CurryingFunctions {
+
+
+
+
+    public static void main(String[] args){
+
+        int [] arr = {1,2,3};
+
+        int n=arr.length;
+
+        int [] newArray = new int[n];
+
+        
+
+    }
+}
