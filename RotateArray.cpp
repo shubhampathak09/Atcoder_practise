5722d28b75c4831e2f5183ba901c384d5099717a
Shubham <95shubham1@gmail.com>
2024-02-02 16:51:58 +0530

    Prepration 2024

---
+++ RotateArray.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void rotate(int arr[],int d,int n){
+	
+	int temp[n];
+	
+	int k=0;
+	
+	int k=0;
+	
+	for(int =d ; i<n;i++){
+		
+		temp[k]=arr[i];
+		k++;
+	}
+	
+	for(int i=0;i<d;i++){
+		temp[k]=arr[i];
+		k++;
+	}
+	
+	for(int i=0;i<n;i++){
+		arr[i] = temp[i];
+	}
+	
+}
+
+void printTheArray(int arr[],int n){
+	for(int i=0; i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+}
+
+int main(){
+	
+	int arr[]= {1,2,3,4,5,6,7};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	int d=2;
+	rotate(arr,d,n);
+	printTheArray(arr,n);
+}
